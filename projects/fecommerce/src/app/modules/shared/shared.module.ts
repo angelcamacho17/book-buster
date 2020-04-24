1d22831c925014ccdac40a37b7e4bc0d750d18a2
Federico Ribero <ribero.federico@outlook.com>
2020-04-24 13:28:21 -0300

    added article

--- projects/fecommerce/src/app/modules/shared/shared.module.ts
+++ projects/fecommerce/src/app/modules/shared/shared.module.ts
@@ -6,29 +6,17 @@ import { FeBottomNavComponent } from '../fe-bottom-nav/fe-bottom-nav.component';
 import { FeCreateButtonComponent } from './components/fe-create-button/fe-create-button.component';
 import { FeCardComponent } from './components/fe-card/fe-card.component';
 import { FeHeaderComponent } from './components/fe-header/fe-header.component';
-<<<<<<< HEAD
 import { FeSearchComponent } from './components/fe-search/fe-search.component';
+import { FeCustomerRowComponent } from './components/fe-customer-row/fe-customer-row.component';
 
 @NgModule({
   declarations: [
     FeBottomNavComponent,
     FeCardComponent,
     FeCreateButtonComponent,
+    FeCustomerRowComponent,
     FeHeaderComponent,
     FeSearchComponent
-=======
-import { FeCustomerRowComponent } from './components/fe-customer-row/fe-customer-row.component';
-
-
-
-@NgModule({
-  declarations: [
-    FeCardComponent,
-    FeCreateButtonComponent,
-    FeSearchComponent,
-    FeHeaderComponent,
-    FeCustomerRowComponent
->>>>>>> 498250c86f59a51136e3b2f9a6bdea072ffc4c46
   ],
   imports: [
     CommonModule,
