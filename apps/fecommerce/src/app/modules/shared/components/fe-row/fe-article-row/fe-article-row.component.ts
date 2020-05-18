7813c462a22ef10ab0ad63ef35fb53116721d6fc
angelcamacho17 <angelcamacho1708@gmail.com>
2020-05-18 12:29:07 -0300

    fixing some bugs in the search after selecting an article

--- apps/fecommerce/src/app/modules/shared/components/fe-row/fe-article-row/fe-article-row.component.ts
+++ apps/fecommerce/src/app/modules/shared/components/fe-row/fe-article-row/fe-article-row.component.ts
@@ -19,7 +19,8 @@ export class FeArticleRowComponent implements OnInit {
   }
 
   openArticleDetail(item) {
-    console.log(item)
-    this._router.navigate(['/article/detail', item.id]);
+    setTimeout(()=> {
+      this._router.navigate(['/article/detail', item.id]);
+    },100)
   }
 }
