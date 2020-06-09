e322c1d63e8ef505354b824261e2af28d86232e4
angelcamacho17 <angelcamacho1708@gmail.com>
2020-06-09 11:06:17 -0300

    fixing scroll in order items

--- apps/fecommerce/src/app/modules/shared/modules/material/material.module.ts
+++ apps/fecommerce/src/app/modules/shared/modules/material/material.module.ts
@@ -28,7 +28,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatAutocompleteModule,
     MatSnackBarModule,
     MatDialogModule,
-    MatProgressBarModule
+    MatProgressBarModule,
   ],
   exports: [
     MatButtonModule,
