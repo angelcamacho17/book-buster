489c01d98f0970fd3f7ac79073b0913522e949c6
angelcamacho17 <angelcamacho1708@gmail.com>
2020-07-03 12:02:46 -0300

    fixing view up bug

--- apps/fecommerce/src/app/modules/shared/components/fe-search/fe-search-results/fe-search-results.component.ts
+++ apps/fecommerce/src/app/modules/shared/components/fe-search/fe-search-results/fe-search-results.component.ts
@@ -10,13 +10,15 @@ export class FeSearchResultsComponent implements OnInit, OnDestroy {
 
   @Input() items: any;
   @Input() rowType: any;
+  public length: number = 0;
   public items$: Observable<any[]> = of(this.items);
 
-  constructor() {}
+  constructor() {
+    this.length = this.items.length;
+  }
 
   ngOnInit(): void {
-    console.log('estoy en search results')
-    console.log(this.items)
+
   }
 
   ngOnDestroy() {
