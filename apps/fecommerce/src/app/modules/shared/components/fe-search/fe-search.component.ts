7f2deeb841146f0d85f3ba5f69f0ae168186c491
Federico Ribero <ribero.federico@outlook.com>
2020-07-07 19:53:25 -0300

    aniamte

--- apps/fecommerce/src/app/modules/shared/components/fe-search/fe-search.component.ts
+++ apps/fecommerce/src/app/modules/shared/components/fe-search/fe-search.component.ts
@@ -37,10 +37,30 @@ export class FeSearchComponent implements OnInit, OnDestroy {
         console.log('windows init')
 
     }
-
-    @HostListener('window:touchmove', ['$event']) onScrollEvent($event) {
-        window.scrollTo(0, 0);
-        console.log('llorcs')
+    private scrollToTop (duration) {
+        // cancel if already on top
+        if (document.scrollingElement.scrollTop === 0) return;
+    
+        const totalScrollDistance = document.scrollingElement.scrollTop;
+        let scrollY = totalScrollDistance, oldTimestamp = null;
+    
+        function step (newTimestamp) {
+            if (oldTimestamp !== null) {
+                // if duration is 0 scrollY will be -Infinity
+                scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+                if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+                document.scrollingElement.scrollTop = scrollY;
+            }
+            oldTimestamp = newTimestamp;
+            window.requestAnimationFrame(step);
+        }
+        window.requestAnimationFrame(step);
+    }
+    @HostListener('window:touchend', ['$event']) onScrollEvent($event) {
+        // window.scrollTo(0, 0);
+        this.scrollToTop(1000);
+        // console.log('llorcs')
+        
     }
 
     private stopScrolling() {
