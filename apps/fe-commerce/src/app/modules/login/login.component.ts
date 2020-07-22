102132d37298cb5d480d1b5150692878ebd668ee
angelcamacho17 <angelcamacho1708@gmail.com>
2020-07-22 15:25:25 -0300

    lintint

--- apps/fe-commerce/src/app/modules/login/login.component.ts
+++ apps/fe-commerce/src/app/modules/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'login',
+  selector: 'fe-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
