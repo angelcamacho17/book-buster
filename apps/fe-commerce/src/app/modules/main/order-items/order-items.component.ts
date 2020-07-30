2b879684bd7900b2de56a5b7784570327f406ca9
angelcamacho17 <angelcamacho1708@gmail.com>
2020-07-30 11:17:22 -0300

    adding bottom offset

--- apps/fe-commerce/src/app/modules/main/order-items/order-items.component.ts
+++ apps/fe-commerce/src/app/modules/main/order-items/order-items.component.ts
@@ -7,9 +7,6 @@ import { startWith, map, switchMap, tap } from 'rxjs/operators';
 import {  MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ArtSheetComponent } from '../shared/components/art-sheet/art-sheet.component';
 
-
-const speed = 10;
-
 @Component({
   selector: 'order-items',
   templateUrl: './order-items.component.html',
@@ -130,7 +127,7 @@ export class OrderItemsComponent implements OnInit, OnDestroy {
 
       }
       this._substractArt = 0;
-      const inputElement: HTMLElement = document.getElementById('card') as HTMLElement;
+      const inputElement: HTMLElement = document.getElementById('content') as HTMLElement;
       setTimeout(() => {
         inputElement.click();
       }, 1);
